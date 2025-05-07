7996905eb1ce1d6008c136519ac034dc2e331a01
Denilson Silva <denilsondeni16@gmail.com>
2025-05-06 21:36:20 -0300

    atualizei o component beneficio/adicionei uma açao para escluir usuarios

--- src/app/funcionalidades/beneficios/beneficios-routing.module.ts
+++ src/app/funcionalidades/beneficios/beneficios-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListaBeneficiosComponent } from './componentes/lista-beneficios/lista-beneficios.component';
+import { DetalheBeneficioComponent } from './componentes/detalhe-beneficio/detalhe-beneficio.component';
 
 const routes: Routes = [
   { path: '', component: ListaBeneficiosComponent }
